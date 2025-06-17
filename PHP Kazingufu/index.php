9a94816fe37bf5b06b41bade945b2d9dfce3eb35
Romuald243 <romykazingufu@gmail.com>
2025-06-17 18:51:40 +0200

    Add files via upload

---
+++ PHP Kazingufu/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Kazingufu Romuald PHP</title>
+</head>
+<body>
+	<h1>Kazingufu Romuald PHP	</h1>
+	<p> ASCII ART  :</p>
+	<pre>
+	**		***
+	**	  ***	
+	**	***
+	****
+	****
+	**	***
+	**	  ***
+	**		***
+	</pre>
+<p> SHA256 of "Kazingufu" is :
+<?php
+echo hash('sha256', 'Kazingufu');
+?>
+</p>
+</body>
+</html>
